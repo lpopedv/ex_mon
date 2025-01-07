d1351936182e9f9b6f22ae10e1a8bc149925dc56
Lucas Pope <lpopedv@proton.me>
2025-01-07 20:43:58 -0300

    feat: computer automatically playing logic

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -5,6 +5,7 @@ defmodule ExMon do
   alias ExMon.Game.{Actions, Status}
 
   @computer_name "Robotinik"
+  @computer_moves [:move_avg, :move_rnd, :move_heal]
 
   def create_player(name, move_avg, move_rnd, move_heal),
     do: Player.build(name, move_avg, move_rnd, move_heal)
@@ -18,19 +19,38 @@ defmodule ExMon do
   end
 
   def make_move(move) do
-    move
-    |> Actions.fetch_move()
-    |> do_move()
+    Game.info()
+    |> Map.get(:status)
+    |> handle_status(move)
   end
 
   defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
 
   defp do_move({:ok, move}) do
     case move do
-      :move_heal -> Actions.heal
+      :move_heal -> Actions.heal()
       move -> Actions.attack(move)
     end
 
     Status.print_round_message(Game.info())
+  end
+
+  defp computer_move(%{turn: :computer, status: :continue}) do
+    {:ok, Enum.random(@computer_moves)}
+    |> do_move()
+  end
+
+  defp computer_move(_), do: :ok
+
+  defp handle_status(:game_over, _move), do: Status.print_round_message(Game.info())
+
+  defp handle_status(_other, move) do
+    Status.print_round_message(Game.info())
+
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+
+    computer_move(Game.info())
   end
 end
